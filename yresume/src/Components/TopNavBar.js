60438612e168a92d8f0a3171fe365a42847704c5
Yuvi <yuvarajselva@hotmail.com>
2021-10-10 11:06:57 +0530

    added navbar

--- yresume/src/Components/TopNavBar.js
+++ yresume/src/Components/TopNavBar.js
@@ -1,38 +1,22 @@
 import React from "react";
 import "bulma/css/bulma.min.css";
-import { Navbar } from 'react-bulma-components';
+import { Navbar } from "react-bulma-components";
 
 function TopNavBar() {
   return (
     // <Section>
-      <Navbar>
-        <Navbar.Brand>
-          <Navbar.Item renderAs="a" href="#">
-            <img
-              src="https://bulma.io/images/bulma-logo.png"
-              alt="Bulma: a modern CSS framework based on Flexbox"
-              width="112"
-              height="28"
-            />
-          </Navbar.Item>
-          <Navbar.Burger />
-        </Navbar.Brand>
-        <Navbar.Menu>
-          <Navbar.Container>
-            <Navbar.Item dropdown hoverable href="#">
-              <Navbar.Link>First</Navbar.Link>
-              <Navbar.Dropdown>
-                <Navbar.Item href="#">Subitem 1</Navbar.Item>
-                <Navbar.Item href="#">Subitem 2</Navbar.Item>
-              </Navbar.Dropdown>
-            </Navbar.Item>
-            <Navbar.Item href="#">Second</Navbar.Item>
-          </Navbar.Container>
-          <Navbar.Container position="end">
-            <Navbar.Item href="#">At the end</Navbar.Item>
-          </Navbar.Container>
-        </Navbar.Menu>
-      </Navbar>
+    <Navbar className="is-black">
+      <Navbar.Menu>
+        <Navbar.Container>
+          <Navbar.Item href="#">HOME</Navbar.Item>
+          <Navbar.Item href="#">ABOUT ME</Navbar.Item>
+          <Navbar.Item href="#">SKILLS</Navbar.Item>
+          <Navbar.Item href="#">PROJECTS</Navbar.Item>
+          <Navbar.Item href="#">AWARDS</Navbar.Item>
+          <Navbar.Item href="#">BLOGS</Navbar.Item>
+        </Navbar.Container>
+      </Navbar.Menu>
+    </Navbar>
     // </Section>
   );
 }
