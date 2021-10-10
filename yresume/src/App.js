a7c9add908f6b6bc831bab5985fc92a7f14fdde5
Yuvi <yuvarajselva@hotmail.com>
2021-10-10 11:37:03 +0530

    added profile component

--- yresume/src/App.js
+++ yresume/src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-import NavigationBar from './Components/TopNavBar';
+import logo from "./logo.svg";
+import "./App.css";
+import NavigationBar from "./Components/TopNavBar";
+import Profile from "./Components/ProfileComponent";
 function App() {
   return (
     <div className="App">
-        <NavigationBar />
-      <header className="App-header">
+      <NavigationBar />
+      <Profile />
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,7 +20,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
---
+++ yresume/src/Components/ProfileComponent.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Image from "react-bootstrap/Image";
+
+function ProfileComponent() {
+  return (
+    <div>
+      <Image src="../../public/img/profile.img/171x180" rounded />
+    </div>
+  );
+}
+
+export default ProfileComponent;
