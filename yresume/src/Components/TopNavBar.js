dd45f2986eddb64efa65f8a060b47f9adc70bb71
Yuvi <yuvarajselva@hotmail.com>
2021-10-10 11:20:17 +0530

    minor change in the navbar

--- yresume/src/Components/TopNavBar.js
+++ yresume/src/Components/TopNavBar.js
@@ -4,20 +4,17 @@ import { Navbar } from "react-bulma-components";
 
 function TopNavBar() {
   return (
-    // <Section>
-    <Navbar className="is-black">
-      <Navbar.Menu>
-        <Navbar.Container>
-          <Navbar.Item href="#">HOME</Navbar.Item>
-          <Navbar.Item href="#">ABOUT ME</Navbar.Item>
-          <Navbar.Item href="#">SKILLS</Navbar.Item>
-          <Navbar.Item href="#">PROJECTS</Navbar.Item>
-          <Navbar.Item href="#">AWARDS</Navbar.Item>
-          <Navbar.Item href="#">BLOGS</Navbar.Item>
-        </Navbar.Container>
-      </Navbar.Menu>
+    <Navbar className="is-black is-fixed-top">
+      <Navbar.Container>
+        <Navbar.Item href="#">HOME</Navbar.Item>
+        <Navbar.Item href="#">ABOUT ME</Navbar.Item>
+        <Navbar.Item href="#">SKILLS</Navbar.Item>
+        <Navbar.Item href="#">PROJECTS</Navbar.Item>
+        <Navbar.Item href="#">AWARDS</Navbar.Item>
+        <Navbar.Item href="#">BLOGS</Navbar.Item>
+          <Navbar.Item href="#">CONTACT</Navbar.Item>
+      </Navbar.Container>
     </Navbar>
-    // </Section>
   );
 }
 
