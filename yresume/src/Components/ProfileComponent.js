bb63737cd4407e9a3768916711511b56484375a8
Yuvi <yuvarajselva@hotmail.com>
2021-10-10 13:14:49 +0530

    added circular to the image

--- yresume/src/Components/ProfileComponent.js
+++ yresume/src/Components/ProfileComponent.js
@@ -1,15 +1,10 @@
 import React from "react";
 import ProfilePicture from "../img/profile.jpg";
-import "react-bulma-components";
+import { Image } from "semantic-ui-react";
+import "semantic-ui-css/semantic.min.css";
+
 function ProfileComponent() {
-  return (
-    <div>
-      {/* <Image src={ProfilePicture} roundedCircle /> */}
-      <figure class="image is-128x128">
-        <img class="is-rounded" src={ProfilePicture}/>
-      </figure>
-    </div>
-  );
+  return <Image src={ProfilePicture} size="medium" circular />;
 }
 
 export default ProfileComponent;
